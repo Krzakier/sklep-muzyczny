52bd4831c6b687829d784fbfe9510fc77526e444
Krzakier <krzakier22@gmail.com>
2026-05-11 09:20:01 +0200

    Add basic App component

---
+++ frontend/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Sklep Muzyczny</h1>
+        <p>Witaj w naszym sklepie</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
